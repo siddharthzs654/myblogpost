b6972b2b0a92e9afe23fb78646f06c6d872602b7
Siddharth Choudhary <siddharth654choudhary@gmail.com>
2019-12-19 04:31:59 +0530

    added models

--- myblog/home/views.py
+++ myblog/home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from . import models
 # Create your views here.
 
 
 def home(request):
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {'post': models.Articles.objects.all()})
 
 
 def base(request):
